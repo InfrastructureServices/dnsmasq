ce3fcc0ec547a522af2fe61761131737b6f08e2f
Petr Menšík <pemensik@redhat.com>
2019-08-28 21:24:25 +0200

    Fix RA unsolicited spam
    Original commit 0a496f059c1e9d75c33cce4c1211d58422ba4f62 introduced
    floods of unsolicited RA spams. It loops between generated and assigned
    address. Compare just subnet and does not check exact address.

--- src/dhcp6.c
+++ src/dhcp6.c
@@ -680,7 +680,8 @@ static int construct_worker(struct in6_addr *local, int prefix,
 	    is_same_net6(local, &template->end6, template->prefix))
 	  {
 	    /* First time found, do fast RA. */
-	    if (template->if_index != if_index || !IN6_ARE_ADDR_EQUAL(&template->local6, local))
+	    if (template->if_index != if_index
+	        || !is_same_net6(&template->local6, local, template->prefix))
 	      {
 		ra_start_unsolicited(param->now, template);
 		param->newone = 1;
